1dbaf99ddfa443db2efe9797655e74c7f26fe6ef
Daniel Kostro <kostro.d@gmail.com>
2013-08-08 17:18:38 -0400

    Interemediate commit for retrieving work at home...

--- SurfFeatures/Logic/testcmd.cxx
+++ SurfFeatures/Logic/testcmd.cxx
@@ -1,50 +1,73 @@
 #include "vtkSlicerSurfFeaturesLogic.h"
 #include "surfLogicUtility.hpp"
 
+// ITK includes
+#include <itkImageFileReader.h>
+#include "itkImageToVTKImageFilter.txx"
+
+// VTK includes
+#include <vtkImageReslice.h>
+
 #include <iostream>
 #include <windows.h>
 
 using namespace std;
 
+// itk typedefs
+typedef unsigned char PixelType;
+const unsigned int    Dimension = 3;
+typedef itk::Image<PixelType, Dimension> ImageType;
+typedef itk::ImageFileReader< ImageType > ReaderType;
+typedef itk::ImageToVTKImageFilter<ImageType> imageToVTKImageFilterType;
+
+
+
+void writeKeypointFromMhaFile(string mhaFile, string outputDir, int minHessian)
+{
+  vtkSlicerSurfFeaturesLogic* surf = vtkSlicerSurfFeaturesLogic::New();
+  surf->setQueryFile(mhaFile);
+  surf->setMinHessian(minHessian);
+  surf->setQueryStartFrame(0);
+  surf->setQueryStopFrame(-1);
+  surf->setRansacMargin(1.5);
+  computeQuery(surf);
+  surf->saveKeypointsAndDescriptors(outputDir,"query");
+
+  // Cropping, calibration matrix and mask will be automatically set
+  surf->Delete();
+}
+
 int main()
 {
+  ReaderType::Pointer reader = ReaderType::New();
+  reader->SetFileName("C:\\Users\\DanK\\MProject\\data\\MNI\\analyze3dus\\1a.3dus");
+
+  imageToVTKImageFilterType::Pointer toVtkFilter = imageToVTKImageFilterType::New();
+  toVtkFilter->SetInput(reader->GetOutput());
+
+  toVtkFilter->Update();
+  vtkImageData* data = toVtkFilter->GetOutput();
+
+  vtkSmartPointer<vtkImageReslice> imageReslice = vtkSmartPointer<vtkImageReslice>::New();
+  imageReslice->SetInput(data);
+  imageReslice->SetResliceAxesDirectionCosines(1,0,0,0,1,0,0,0,1);
+  imageReslice->SetResliceAxesOrigin(0,0,0);
+  vtkImageData* reslicedImage = imageReslice->GetOutput();
+
+  // Convert the image to opencv
+
+
+  // Compute the features on the resliced image
+
   vector<string> files;
   vector<string> outputDirs;
   readLines("C:\\Users\\DanK\\MProject\\data\\MNI_AMIGO_mha.txt",files);
   readLines("C:\\Users\\DanK\\MProject\\data\\MNI_AMIGO_dirs.txt",outputDirs);
+  int minHessian = 400;
 
   if(files.size() != outputDirs.size())
     return 0;
+
   for(int i=0; i<files.size(); i++)
-  {
-    vtkSlicerSurfFeaturesLogic* surf = vtkSlicerSurfFeaturesLogic::New();
-    surf->setQueryFile(files[i]);
-    surf->setMinHessian(400);
-    surf->setQueryStartFrame(0);
-    surf->setQueryStopFrame(-1);
-    surf->setRansacMargin(1.5);
-    vtkSmartPointer<vtkMatrix4x4> matrix = vtkSmartPointer<vtkMatrix4x4>::New();
-    matrix->Identity();
-    surf->setImageToProbeTransform(matrix);
-    computeQuery(surf);
-    //std::string dir, file, ext;
-    //splitDir(files[i],dir,file,ext);
-    surf->saveKeypointsAndDescriptors(outputDirs[i],"query");
-
-
-    surf->Delete();
-  }
-
-
-  /*cv::FileStorage fs("C:\\Users\\DanK\\test.yml", cv::FileStorage::READ);
-  vector<cv::KeyPoint> readKey;
-  cv::Mat readDescriptor;
-  if(fs.isOpened())
-  {
-    read(fs["keyPoints"],readKey);
-    read(fs["descriptors"],readDescriptor);
-  }
-  fs.release();
-  cout << readKey.size() << endl;
-  cout << readDescriptor.rows << " " << readDescriptor.cols << endl;*/
+    writeKeypointsFromMhaFile(files[i], outputDirs[i], minHessian);
 }
