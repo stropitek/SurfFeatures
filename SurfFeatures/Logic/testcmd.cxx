246ae5b8b83b18f93680aa7d2e311fe40d16c9d0
Daniel Kostro <kostro.d@gmail.com>
2013-08-09 20:44:57 -0400

    Intermediate changes... for continuing at home

--- SurfFeatures/Logic/testcmd.cxx
+++ SurfFeatures/Logic/testcmd.cxx
@@ -9,6 +9,7 @@
 #include <vtkImageReslice.h>
 
 #include <iostream>
+#include <itkNiftiImageIO.h>
 
 using namespace std;
 
@@ -39,34 +40,62 @@ void writeKeypointsFromMhaFile(string mhaFile, string outputDir, int minHessian)
 int main()
 {
   ReaderType::Pointer reader = ReaderType::New();
-  reader->SetFileName("C:\\Users\\DanK\\MProject\\data\\MNI\\analyze3dus\\1a.3dus");
+  reader->SetImageIO(itk::NiftiImageIO::New());
+  reader->SetFileName("C:/Users/DanK/MProject/data/MNI/analyze3dus/1a.3dus.nii");
+  reader->Update();
+  ImageType::Pointer itkImage = reader->GetOutput();
 
   imageToVTKImageFilterType::Pointer toVtkFilter = imageToVTKImageFilterType::New();
   toVtkFilter->SetInput(reader->GetOutput());
 
   toVtkFilter->Update();
   vtkImageData* data = toVtkFilter->GetOutput();
+  int* dim = data->GetDimensions();
 
   vtkSmartPointer<vtkImageReslice> imageReslice = vtkSmartPointer<vtkImageReslice>::New();
   imageReslice->SetInput(data);
+  //imageReslice->SetInformation(data->GetInformation());
   imageReslice->SetResliceAxesDirectionCosines(1,0,0,0,1,0,0,0,1);
-  imageReslice->SetResliceAxesOrigin(0,0,0);
+  imageReslice->SetResliceAxesOrigin(213.,33.,-236.8);
+  imageReslice->SetOutputDimensionality(2);
+  imageReslice->Update();
   vtkImageData* reslicedImage = imageReslice->GetOutput();
+  vtkIdType id = reslicedImage->FindPoint(213.,33.,-230.);
+  vtkIdType nbPoints = reslicedImage->GetNumberOfPoints();
+  double* center = reslicedImage->GetCenter();
+  string s1 = data->GetScalarTypeAsString();
+  string s2 = reslicedImage->GetScalarTypeAsString();
+  unsigned char* reslicedPointer = (unsigned char*)reslicedImage->GetScalarPointer();
+  unsigned char* dataPointer = (unsigned char*)data->GetScalarPointer();
+  vtkIdType* increments = data->GetIncrements();
+  int* dims = data->GetDimensions();
+  data->SetSpacing(-0.3,0.3,0.3);
+  for(vtkIdType i=0; i<data->GetNumberOfPoints(); i++)
+  {
+    double* pos = data->GetPoint(i);
+    if(dataPointer[i] != 0){
+      cout << "hello";
+    }
+  }
 
   // Convert the image to opencv
 
 
   // Compute the features on the resliced image
 
-  vector<string> files;
-  vector<string> outputDirs;
-  readLines("C:\\Users\\DanK\\MProject\\data\\MNI_AMIGO_mha.txt",files);
-  readLines("C:\\Users\\DanK\\MProject\\data\\MNI_AMIGO_dirs.txt",outputDirs);
-  int minHessian = 400;
 
-  if(files.size() != outputDirs.size())
-    return 0;
+  // ==============================================
+  // write correspondences for a set of images
+  // ==============================================
+  //vector<string> files;
+  //vector<string> outputDirs;
+  //readLines("C:\\Users\\DanK\\MProject\\data\\MNI_AMIGO_mha.txt",files);
+  //readLines("C:\\Users\\DanK\\MProject\\data\\MNI_AMIGO_dirs.txt",outputDirs);
+  //int minHessian = 400;
 
-  for(int i=0; i<files.size(); i++)
-    writeKeypointsFromMhaFile(files[i], outputDirs[i], minHessian);
+  //if(files.size() != outputDirs.size())
+  //  return 0;
+
+  //for(int i=0; i<files.size(); i++)
+  //  writeKeypointsFromMhaFile(files[i], outputDirs[i], minHessian);
 }
